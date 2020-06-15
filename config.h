40c95c0c8ae037899c3119f964d3519a0663333a
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-06-14 21:34:51 -0700

    Apply selectioncolors patch

--- config.h
+++ config.h
@@ -127,10 +127,9 @@ unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
-// TODO: apply selection colors patch
-// unsigned int selectionbg = 258;
-// unsigned int selectionfg = 7;
-// static int ignoreselfg = 1;
+unsigned int selectionbg = 258;
+unsigned int selectionfg = 7;
+static int ignoreselfg = 1;
 
 /*
  * Default shape of cursor
